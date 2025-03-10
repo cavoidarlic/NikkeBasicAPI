198f4781f4a5d6dbac9340435c26406d7a32b8cc
Cavoid <79347941+cavoidarlic@users.noreply.github.com>
2025-03-10 07:32:34 +0700

    Update nikke_r.py

--- nikke_r.py
+++ nikke_r.py
@@ -7,7 +7,8 @@ R_CHARACTERS = {
         "element": "Water",
         "class": "Supporter",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c305_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c305_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/5/5e/IDoll_Ocean_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052345"
     },
     "product 23": {
         "name": "Product 23",
@@ -17,7 +18,8 @@ R_CHARACTERS = {
         "element": "Wind",
         "class": "Supporter",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/8/83/Product_23_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052429"
     },
     "soldier fa": {
         "name": "Soldier FA",
@@ -27,7 +29,8 @@ R_CHARACTERS = {
         "element": "Iron",
         "class": "Defender",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c301_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c301_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/e/e0/Soldier_FA_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052448"
     },
     "idoll flower": {
         "name": "iDoll Flower",
@@ -37,7 +40,8 @@ R_CHARACTERS = {
         "element": "Wind",
         "class": "Defender",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c304_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c304_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/d/d1/IDoll_Flower_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052344"
     },
     "idoll sun": {
         "name": "iDoll Sun",
@@ -47,7 +51,8 @@ R_CHARACTERS = {
         "element": "Iron",
         "class": "Supporter",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c308_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c308_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/e/e6/IDoll_Sun_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052346"
     },
     "product 08": {
         "name": "Product 08",
@@ -57,7 +62,8 @@ R_CHARACTERS = {
         "element": "Electric",
         "class": "Supporter",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c302_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c302_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/8/8f/Product_08_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052427"
     },
     "product 12": {
         "name": "Product 12",
@@ -67,26 +73,29 @@ R_CHARACTERS = {
         "element": "Wind",
         "class": "Attacker",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/8/85/Product_12_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052428"
     },
-    "product 23": {
-        "name": "Product 23",
-        "weapon": "SG",
-        "manufacturer": "Missilis",
-        "burst": "II",
-        "element": "Wind",
+    "soldier eg": {
+        "name": "Soldier EG",
+        "weapon": "AR",
+        "manufacturer": "Elysion",
+        "burst": "III",
+        "element": "Electric",
+        "class": "Attacker",
+        "rarity": "R",
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c300_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/8/8c/Soldier_EG_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052447"
+    },
+    "soldier ow": {
+        "name": "Soldier OW",
+        "weapon": "SMG",
+        "manufacturer": "Elysion",
+        "burst": "I",
+        "element": "Fire",
         "class": "Supporter",
         "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp"
-    },
-    "product 23": {
-        "name": "Product 23",
-        "weapon": "SG",
-        "manufacturer": "Missilis",
-        "burst": "II",
-        "element": "Wind",
-        "class": "Supporter",
-        "rarity": "R",
-        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c307_00_s.webp"
+        "icon_url": "https://static.dotgg.gg/nikke/characters/si_c306_00_s.webp",
+        "image_url": "https://static.wikia.nocookie.net/nikke-goddess-of-victory-international/images/a/a2/Soldier_OW_MI.png/revision/latest/thumbnail-down/width/200/height/200?cb=20231123052448"
     },
 }
